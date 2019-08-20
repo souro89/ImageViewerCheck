9d9eaba8d83ae2438349fd46c5bfab6facfa84ac
souro89 <souro89@gmail.com>
2019-08-20 18:03:22 +0400

    Adding Menu

--- src/common/header/header.js
+++ src/common/header/header.js
@@ -1,16 +1,34 @@
 import React, { Component } from "react";
 import "./header.css";
 import SearchIcon from "@material-ui/icons/Search";
-import { Input, FormControl } from "@material-ui/core";
+import { Input, FormControl, Menu, MenuItem } from "@material-ui/core";
 
 class Header extends Component {
+  constructor() {
+    super();
+    this.state = {
+      showDropdown: false
+    };
+  }
+
+  profileIconClickHandler = e => {
+    // this.state.showDropdown === true
+    //   ? this.setState({ showDropdown: false })
+    //   : this.setState({ showDropdown: true });
+  };
+
   render() {
     return (
       <div>
         {this.props.showSearchBarAndProfileIcon === "true" ? (
           <header className="app-header">
             <span className="app-logo">Image Viewer</span>
-            <span className="profile-icon" />
+            <span
+              className="profile-icon"
+              aria-controls="simple-menu"
+              aria-haspopup="true"
+              onClick={this.profileIconClickHandler}
+            />
             <span className="search-bar">
               <SearchIcon className="icon-search" />
               <FormControl>
