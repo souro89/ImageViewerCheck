5f90bb8f58081017c0de0b7a5663ced6a80b05db
souro89 <souro89@gmail.com>
2019-08-20 21:28:26 +0400

    Getting Data

--- src/screens/home/home.js
+++ src/screens/home/home.js
@@ -6,16 +6,32 @@ class Home extends Component {
   constructor() {
     super();
     this.state = {
-      loggedIn: sessionStorage.getItem("access-token") == null ? true : false
+      loggedIn: sessionStorage.getItem("access-token") == null ? false : true
     };
   }
 
   componentWillMount() {
-    if (this.state.loggedIn === true) {
+    if (!this.state.loggedIn === true) {
       this.props.history.push({
         pathname: "/"
       });
     }
+
+    let that = this;
+    let data = null;
+    let xhr = new XMLHttpRequest();
+    let url =
+      "https://api.instagram.com/v1/users/self/?access_token=" +
+      sessionStorage.getItem("access-token");
+
+    xhr.open("GET", url);
+    xhr.setRequestHeader("cache-control", "no-cache");
+    xhr.send(data);
+    xhr.addEventListener("readystatechange", function() {
+      if (this.readyState === 4) {
+        console.log(this.responseText);
+      }
+    });
   }
 
   logoutHandler = () => {
